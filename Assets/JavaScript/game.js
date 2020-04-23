e52840acd08141647bc1e46a54c8d57eceba7f3a
Joe Warren <joseph@cheyankee.com>
2020-04-23 15:48:39 -0700

    Have adjusted java to work for wins, losses, and partially for yourguesses

--- Assets/JavaScript/game.js
+++ Assets/JavaScript/game.js
@@ -4,34 +4,23 @@ var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','
 
 var wins = 0; 
 var losses = 0;
-var guesses = 9;
+var guesses = 0;
 
-var computerChoice = letters[Math.floor(Math.random() * letters.length)];
+computerChoice = letters[Math.floor(Math.random() * letters.length)];
     console.log(computerChoice)
 
-function yourguess(){
-    document.getElementById("yourguesses").innerhtml = 
-}    
-
-function updateScore() {
-        document.getElementById("#score").innerHTML = "Score: " + score;
-      }
 
 document.onkeypress = function(event) {
-    var userGuess = event.key;
+    var userGuess = event.key; 
     
     if(userGuess === computerChoice){
         wins++;
     }else{
-        guesses--;
+        losses++;
     }
-    
-    if(guesses === 0){
-        losses++
-    }
-    
+
     document.getElementById('wins').innerHTML = "Wins: " + wins;
     document.getElementById('losses').innerHTML = "Losses: " + losses;
-    document.getElementById('guesses').innerHTML = "Guesses" + guesses;
+    document.getElementById('yourguesses').innerHTML = "Your guesses so far: " + event.key;
 }
 
