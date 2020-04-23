1fb24c475407d6752e6ed0d2164a627f8fdd696b
Joe Warren <joseph@cheyankee.com>
2020-04-22 17:16:53 -0700

    initial outline of javascript and html.

---
+++ Assets/JavaScript/game.js
@@ -0,0 +1,37 @@
+
+
+var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+var wins = 0; 
+var losses = 0;
+var guesses = 9;
+
+var computerChoice = letters[Math.floor(Math.random() * letters.length)];
+    console.log(computerChoice)
+
+function yourguess(){
+    document.getElementById("yourguesses").innerhtml = 
+}    
+
+function updateScore() {
+        document.getElementById("#score").innerHTML = "Score: " + score;
+      }
+
+document.onkeypress = function(event) {
+    var userGuess = event.key;
+    
+    if(userGuess === computerChoice){
+        wins++;
+    }else{
+        guesses--;
+    }
+    
+    if(guesses === 0){
+        losses++
+    }
+    
+    document.getElementById('wins').innerHTML = "Wins: " + wins;
+    document.getElementById('losses').innerHTML = "Losses: " + losses;
+    document.getElementById('guesses').innerHTML = "Guesses" + guesses;
+}
+
